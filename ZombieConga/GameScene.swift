b0ae5067c831c02a57833e6d3bca87ff78475c54
Michael Vilabrera <m.vilabrera@gmail.com>
2015-05-05 14:18:35 -0400

    chapter one commence

---
+++ ZombieConga/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  ZombieConga
+//
+//  Created by Michael Vilabrera on 5/5/15.
+//  Copyright (c) 2015 Giving Tree. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        backgroundColor = SKColor.whiteColor()
+    }
+}

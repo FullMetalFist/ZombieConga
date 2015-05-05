fc387b2912aba06ceedc0e424037a7f6582d3beb
Michael Vilabrera <m.vilabrera@gmail.com>
2015-05-05 14:49:05 -0400

    some discoveries in chapter one

--- ZombieConga/GameScene.swift
+++ ZombieConga/GameScene.swift
@@ -11,5 +11,18 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
         backgroundColor = SKColor.whiteColor()
+        let background = SKSpriteNode(imageNamed: "background1")
+//        background.position = CGPoint(x: size.width/2, y: size.height/2)
+        background.anchorPoint = CGPointZero
+        background.position = CGPointZero
+//        background.zRotation = CGFloat(M_PI)/8
+        
+        // interesting discovery...
+        // remove the anchorPoint & position,
+        // with the CGPoint set to center- the
+        // background spins across the center.
+        // otherwise, the background spins depending on
+        // where the anchor point is placed.
+        addChild(background)
     }
 }
